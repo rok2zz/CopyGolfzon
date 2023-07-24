43c600ee5baeda7c0fd01b3751f2604f66ee84e1
[Chaerok] <[rhffpa23@gmail.com]>
2023-07-24 12:41:23 +0900

    register

---
+++ src/screens/rootStack/mainTab/screens/homeTab/screens/Field.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import WebViewComponent from '../../../../../../components/WebViewComponent';
+
+function Field(): JSX.Element {
+    return (
+        <WebViewComponent uri='https://www.theswingblack.co.kr/' />
+    )
+}
+
+export default Field
